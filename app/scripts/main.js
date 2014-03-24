c9ff0b8aec9177d5a64f06709883530900bde31c
Poltergeist <spectator.networx@gmail.com>
2014-03-24 17:53:09 +0100

    add play and pause function.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,20 @@
 var text = 'Lorem Ipsum dolor Zit amet.',
-    $display = document.querySelector('#display');
+    $display = document.querySelector('#display'),
+    timer;
 text = text.split(' ');
 function next(){
   if(text.length > 1){
     $display.innerHTML = text.shift();
-    setTimeout(next, 60000 / 250)
+    timer = setTimeout(next, 60000 / 250)
   }
 }
 next();
+
+document.body.addEventListener('click', function(){
+  if(timer) {
+    timer = clearTimeout(timer);
+    return;
+  }
+  timer = setTimeout(next, 60000 / 250)
+});
+
